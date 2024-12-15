7151463e3c0817ab421e3166170eddb54031b67a
VideoKompany <135313844+VlaDOS1408@users.noreply.github.com>
2024-12-15 23:43:02 +0400

    Добавьте файлы проекта.

---
+++ DiscordSpecialBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DiscordSpecialBot
+{
+    internal class Program
+    {
+        private readonly static DiscordSocketClient _client = new();
+
+        private readonly static int _indexOfTokenInLaunchArgs = 0;
+
+        private static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new Exception("Please specify the bot token in the launch arguments.");
+            }
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, args[_indexOfTokenInLaunchArgs]);
+
+            await _client.StartAsync();
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
